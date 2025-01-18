882e4487d9538a894a934497163837005d17ee93
Ivaylo1992 <ivailop123@gmail.com>
2025-01-18 06:31:24 +0200

    added login functionality and tests

--- simpleblog/accounts/views.py
+++ simpleblog/accounts/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import redirect, render
 from django.urls import reverse
 from simpleblog.accounts.forms import UserRegistrationForm
-
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import authenticate, login as auth_login
 
 def signup(request):
     form = UserRegistrationForm()
@@ -23,4 +24,25 @@ def signup(request):
 
 
 def login(request):
-    return render(request, 'accounts/login.html')
+    form = AuthenticationForm()
+
+    if request.method == 'POST':
+        form = AuthenticationForm(request, request.POST)
+
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+
+            user = authenticate(username=username, password=password)
+
+            if user is not None:
+                auth_login(request, user)
+                
+                return redirect(reverse('homepage'))
+
+
+    context = {
+        'form': form
+    }
+
+    return render(request, 'accounts/login.html', context)
